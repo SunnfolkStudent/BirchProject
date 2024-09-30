b6b6041d9d03884fe0ae73f14599e3864d71e08d
Drianna-79 <sofmolnet@gmail.com>
2024-09-30 15:07:56 +0200

    Buildup for startmenu
    I tried to make it somewhat work, but I need to figure it out more

---
+++ Assets/Scripts/Player/StartController.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class StartController : MonoBehaviour
+{
+    //Make it check for input AND also the current scene, if it is the start menu then it will go to gameplay mode
+}
