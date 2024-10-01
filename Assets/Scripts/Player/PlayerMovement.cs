d576ce9bfb72152df22bbb23ef5c56819cf3e926
Drianna-79 <sofmolnet@gmail.com>
2024-10-01 11:21:01 +0200

    Start menu + shooting
    Sir Markus the Code Wizard saves the day!!!

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,23 +8,56 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
 
     public GameObject halo;
-    public Transform haloSpawn;
+    public Vector2 haloSpawn;
+
+    public float haloSpeed;
+    public float spawnTime;
+    private float spawnTimeCounter;
+
+    private Vector2 _lookVector;
     
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _input = GetComponent<Input_Actions>();
+
+        if (_lookVector == Vector2.zero)
+        {
+            _lookVector.x = 1;
+        }
     }
-
-
+    
     void Update()
     {
         if (_input.Throw)
         {
-            Instantiate(halo, haloSpawn.position, Quaternion.identity);
+            if (!(Time.time > spawnTimeCounter)) return; 
+            var projectileClone = Instantiate(halo, UpdateSpawnPosition(), Quaternion.identity);
+            projectileClone.GetComponent<Rigidbody2D>().linearVelocity =
+                _lookVector * haloSpeed + _rigidbody2D.linearVelocity;
+            
+            spawnTimeCounter = Time.time + spawnTime;
+            Destroy(projectileClone, 4f);
+        }
+
+        if (_input.MoveDirection != Vector2.zero)
+        {
+            _lookVector = _input.MoveDirection;
         }
     }
 
+    private Vector2 UpdateSpawnPosition()
+    {
+        haloSpawn.x = transform.localPosition.x + (_input.MoveDirection.x / 2);
+        haloSpawn.y = transform.localPosition.y + (_input.MoveDirection.y / 2);
+
+        if (haloSpawn == Vector2.zero)
+        {
+            haloSpawn.x = transform.localPosition.x + 0.5f;
+        }
+        return haloSpawn;
+    }
+    
     private void FixedUpdate()
     {
         _rigidbody2D.linearVelocity = _input.MoveDirection * moveSpeed;
--- Assets/Scripts/Player/StartController.cs
+++ Assets/Scripts/Player/StartController.cs
@@ -1,6 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class StartController : MonoBehaviour
 {
     //Make it check for input AND also the current scene, if it is the start menu then it will go to gameplay mode
+    void Update()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "StartMenuScene" && Keyboard.current.anyKey.wasPressedThisFrame)
+        {
+            Debug.Log("Input Detected!");
+            SceneManager.LoadScene("MagTestScene");
+        }
+    }
 }
