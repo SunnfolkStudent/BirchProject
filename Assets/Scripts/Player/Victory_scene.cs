558de439ffb3bc5f711773f0cbffa7d523fb6bd1
hannesivesel <hannes.iversen.selstad@gmail.com>
2024-10-02 19:01:31 +0200

    Victory scene added and sends you to intro

---
+++ Assets/Scripts/Player/Victory_scene.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Victory_scene : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(SendToStart());
+    }
+
+    private IEnumerator SendToStart()
+    {
+        yield return new WaitForSeconds(10f);
+        SceneManager.LoadScene("IntroCutscene");
+    }
+    
+}
