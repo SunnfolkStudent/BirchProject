f06429fcf5936a0c541da4c1e358eb1b2de33830
hannesivesel <hannes.iversen.selstad@gmail.com>
2024-10-02 18:28:53 +0200

    Halo hit and enemy dying sound effects

--- Assets/Scripts/Player/HaloCollision.cs
+++ Assets/Scripts/Player/HaloCollision.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class KillBoxController : MonoBehaviour
 {
+    [Header("Audio")]
+    
+    
+    public AudioClip[] enemyDeath;
+    public AudioClip haloHit;
+    //public AudioClip[] coolLineSound;
+    
+    [Space(5)] 
+  
+    private AudioSource _audioSource;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        _audioSource = GetComponent<AudioSource>();
         //if other object is a killbox
         if (other.gameObject.CompareTag("KillBox"))
         {
@@ -13,8 +24,12 @@ public class KillBoxController : MonoBehaviour
         
         if (other.gameObject.layer == 3)
         {
+            _audioSource.PlayOneShot(haloHit);
+            _audioSource.PlayOneShot(enemyDeath[Random.Range(0, enemyDeath.Length)]);
             Destroy(other.gameObject);
             Destroy(this.gameObject);
+            
         }
     }
+    
 }
