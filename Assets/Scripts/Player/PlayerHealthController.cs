1489f114ea4e796906d18cc3056509e85de17520
Drianna-79 <sofmolnet@gmail.com>
2024-10-02 14:38:25 +0200

    Health UI

---
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public PlayerCollision target;
+    public Image[] hearts;
+    
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < target.playerHealth)
+            {
+                hearts[i].color = new Color(1, 0, 0, 1);
+            }
+            else
+            {
+                hearts[i].color = new Color(1, 1, 1, 0.5f);
+            }
+        }
+    }
+}
