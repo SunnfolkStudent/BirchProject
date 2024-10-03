ac0cc74227f458802e5101b796e530d90abcbaf5
Magnus <magnus.nistad.helvig@gmail.com>
2024-10-03 17:50:26 +0200

    Height Meter

---
+++ Assets/HeighMeterController.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+
+public class HeighMeterController : MonoBehaviour
+{
+    private int height;
+    public TextMeshProUGUI textMesh;
+
+    void Update()
+    {
+        //about 140 seconds to the top. at 7.5 per second it reaches 1000m at the top
+        height = Mathf.RoundToInt(Time.time * 7.5f);
+        textMesh.text = height.ToString() + "M";
+    }
+}
