b0c754ef9b791b60dd0524d413abaa6f4a586802
hannesivesel <hannes.iversen.selstad@gmail.com>
2024-10-03 11:07:27 +0200

    Update PlayerAnimationController.cs

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 
-public class BocAnimationController : MonoBehaviour
+public class PlayerAnimationController : MonoBehaviour
 {
     public Rigidbody2D rb2D;
     public Animator _animator;
@@ -9,6 +9,7 @@ public class BocAnimationController : MonoBehaviour
     public float animationCooldown = 0;
     
     private Input_Actions _input;
+    private Vector2 _previousShootDirection;
 
     void Start()
     {
@@ -22,19 +23,39 @@ public class BocAnimationController : MonoBehaviour
         {
             //Half Second cooldown on attacks
             animationCooldown = Time.time + 0.5f;
-            
-            if (rb2D.linearVelocityY > Vector2.zero.y)
-            {
-                _animator.Play("Boc_Up_Throw");
-            }
 
-            else if (rb2D.linearVelocityY < Vector2.zero.y)
+            if (_input.MoveDirection == Vector2.zero)
             {
-                _animator.Play("Boc_Down_Throw");
+                if (_previousShootDirection == Vector2.up)
+                {
+                    _animator.Play("Boc_Up_Throw");
+                }
+                else if (_previousShootDirection == Vector2.down)
+                {
+                    _animator.Play("Boc_Down_Throw");
+                }
+                else
+                {
+                    _animator.Play("Boc_Side_Throw");
+                }
             }
             else
             {
-                _animator.Play("Boc_Side_Throw");
+                if (_input.MoveDirection.y > 0)
+                {
+                    _animator.Play("Boc_Up_Throw");
+                    _previousShootDirection = Vector2.up;
+                }
+                else if (_input.MoveDirection.y < 0)
+                {
+                    _animator.Play("Boc_Down_Throw");
+                    _previousShootDirection = Vector2.down;
+                }
+                else
+                {
+                    _animator.Play("Boc_Side_Throw");
+                    _previousShootDirection = Vector2.right;
+                }
             }
         }
         else
