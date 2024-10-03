99cfe50940b791e063e26b8a27d966ada0eaf73a
hannesivesel <hannes.iversen.selstad@gmail.com>
2024-10-03 12:07:05 +0200

    Music added!!!

---
+++ Assets/Scripts/Player/AudioManager.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+   private AudioSource _audioSource;
+
+   public AudioClip[] musicClips;
+
+   private void Start()
+   {
+      _audioSource = GetComponent<AudioSource>();
+   }
+
+   private void Update()
+   {
+      UpdateMusic(Time.time);
+   }
+
+   void UpdateMusic(float time)
+   {
+      if (time < 70 && _audioSource.clip != musicClips[0])
+      {
+         _audioSource.clip = musicClips[0];
+         _audioSource.Play();
+      }
+     else if (time > 70 && _audioSource.clip == musicClips[0])
+     {
+        _audioSource.clip = musicClips[1];
+        _audioSource.loop = false;
+        _audioSource.Play();
+        _audioSource.volume += 0.1f;
+     }
+      else if(_audioSource.isPlaying == false)
+      {
+           _audioSource.clip = musicClips[2];
+           _audioSource.loop = true;
+           _audioSource.Play();
+           _audioSource.volume -= 0.1f;
+      }
+   }
+}
