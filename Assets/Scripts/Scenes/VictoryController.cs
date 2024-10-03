d4a5c1e55a5bc267024061a9314cd75f1df7ce74
hannesivesel <hannes.iversen.selstad@gmail.com>
2024-10-03 18:07:27 +0200

    victory screen sparkle

--- Assets/Scripts/Scenes/VictoryController.cs
+++ Assets/Scripts/Scenes/VictoryController.cs
@@ -6,19 +6,29 @@ using UnityEngine.SceneManagement;
 public class VictoryController : MonoBehaviour
 {
     private AudioSource _audioSource;
-
-    public AudioClip VictoryMusic;
+    public AudioClip sparkleSound;
+    public AudioClip victoryMusic;
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
-        _audioSource.PlayOneShot(VictoryMusic);
+        _audioSource.PlayOneShot(victoryMusic);
+        StartCoroutine(StartSparkle());
         StartCoroutine(SendToStart());
     }
 
     private IEnumerator SendToStart()
     {
-        yield return new WaitForSeconds(12f);
+        yield return new WaitForSeconds(12.5f);
         SceneManager.LoadScene("StartMenuScene");
+    }
+    
+    private IEnumerator StartSparkle()
+    {
+        yield return new WaitForSeconds(2.5f);
+        _audioSource.clip = sparkleSound;
+        _audioSource.loop = true;
+        _audioSource.Play();
+        
     }
     
 }
